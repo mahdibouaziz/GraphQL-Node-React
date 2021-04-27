49dde20cad6aafbf18bb1ed1e0c7cbaf9a6b1277
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2021-04-27 18:44:55 +0100

    able to add a book in the mongodb with graphQL

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,62 +1,75 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
-import { getAuthorsQuery } from "../queries/queries";
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import {
+  getAuthorsQuery,
+  addBookMutation,
+  getBooksQuery,
+} from "../queries/queries";
 
 export default function AddBook() {
   const { loading, error, data } = useQuery(getAuthorsQuery);
+
+  const [bookName, setBookName] = useState("");
+  const [genre, setGenre] = useState("");
+  const [authorId, setAuthorId] = useState("");
+
+  // eslint-disable-next-line no-unused-vars
+  const [addBookMut, { dataMutation }] = useMutation(addBookMutation);
+
+  const submitForm = () => {
+    addBookMut({
+      variables: {
+        name: bookName,
+        genre: genre,
+        authorId: authorId,
+      },
+      refetchQueries: [{ query: getBooksQuery }],
+    });
+  };
 
   return (
     <form id="add-book">
       <div className="field">
         <label>Book name:</label>
-        <input type="text" />
+        <input
+          value={bookName}
+          onChange={(e) => setBookName(e.target.value)}
+          type="text"
+        />
       </div>
 
       <div className="field">
         <label>Genre:</label>
-        <input type="text" />
+        <input
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+          type="text"
+        />
       </div>
 
       <div className="field">
         <label>Author:</label>
-        <select>
+        <select value={authorId} onChange={(e) => setAuthorId(e.target.value)}>
           {loading ? <option>Loading...</option> : null}
           {error ? <option>error...</option> : null}
           {data
             ? data.authors.map((author) => (
-                <option key={author.id}>{author.name}</option>
+                <option key={author.id} value={author.id}>
+                  {author.name}
+                </option>
               ))
             : null}
         </select>
       </div>
 
-      <button>Add a book</button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          submitForm();
+        }}
+      >
+        Add a book
+      </button>
     </form>
   );
 }
-
-/* 
-<form id="add-book">
-
-        <div className="field">
-          <label>Book name:</label>
-          <input type="text"/>
-        </div>
-        
-        <div className="field">
-          <label>Genre:</label>
-          <input type="text"/>
-        </div>
-
-        <div className="field">
-          <label>Author:</label>
-          <select>
-            <option>Select Author</option>
-          </select>
-        </div>
-
-        <button></button>
-
-      </form>
-
-*/
